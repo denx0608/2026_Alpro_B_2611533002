57d590361c628951bbef08494e4d1a927ca2d83f
denx0608 <mdanishabshori@gmail.com>
2026-09-10 09:46:53 +0700

    Add files via upload

---
+++ pekan2/Input_2611533002.py
@@ -0,0 +1,10 @@
+# Buat file dengan nama Input_2611533002.py
+# Buat Program untuk menghitung luas persegi panjang
+# Nama variable ditambah 4 digit nim terakhir contoh: panjang_1234
+# Program ini menggunakan fungsi input() untuk meminta pengguna
+# memasaukan nilai panjang dan lebar persegi panjang
+# Nilai yang dimasukin akan dikonversi menjadi tipe data integer
+panjang_3002 = input('6')
+lebar_3002 = input('8')
+luas_3002 = int(panjang_3002) * int(lebar_3002)
+print("luas = ", luas_3002)
