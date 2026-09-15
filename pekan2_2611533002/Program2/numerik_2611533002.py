9a4db98bf152902ba0b4f7bdceadc9db125cce3d
denx0608 <mdanishabshori@gmail.com>
2026-09-15 21:23:05 +0700

    Add files via upload

---
+++ pekan2_2611533002/Program2/numerik_2611533002.py
@@ -0,0 +1,13 @@
+# Buat dile dengan nama Numerik_22611533002.py
+#create a variabel with integer float value
+a_3002=100
+print("The type of variable having value", a_3002, " is ", type(a_3002))
+
+#create a variable with float value.0
+b_3002=10.2345
+print("The type of variable having valeu", b_3002, " is ", type(b_3002))
+
+#create a variable with complex value.
+
+c_3002=100+3j
+print("The type of variable having value", c_3002, " is ", type(c_3002))
